2ba632377dae31913f3801881b39c944d3285fca
Sandra.xn <sandrachebet003@gmail.com>
2025-10-01 19:52:07 +0300

    Add files via upload

---
+++ attendance.c
@@ -0,0 +1,23 @@
+//HELLO WORLD
+/*
+name:SANDRAH CHEBET
+Reg:PA106/G/28741/25
+Date:30/09/2025
+*/
+
+#include<stdio.h>
+int main(){
+    int  attendance,marks;
+    printf("\nENTER YOUR ATTENDANCE: ");
+    scanf("%d",&attendance);
+    printf("\nENTER YOUR MARKS: ");
+    scanf("%d",&marks);
+    if(attendance>=75&&marks>=40){
+     printf("you are eligible for final exams");
+     }
+     else
+     {
+     printf("you are not eligible for final exams");
+     }
+     return 0;
+ }    
